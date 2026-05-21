b69c59a649c7c93b6fcec0bb1af8c95eeb44eed6
Semion Medvedev <126599206+Fuinny@users.noreply.github.com>
2026-05-21 17:07:50 +0300

    Update migrations

--- database/migrations/2026_05_19_201652_create_budgets_table.php
+++ database/migrations/2026_05_19_201652_create_budgets_table.php
@@ -16,8 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->decimal('amount_limit', 8, 2);
-            $table->unsignedTinyInteger('month');
-            $table->unsignedTinyInteger('year');
+            $table->date('budget_date');
             $table->timestamps();
         });
     }
